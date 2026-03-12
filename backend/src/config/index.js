0c39759ade886eccf881d420742ee9c47c453b47
Hiagyl <joshuaticot@gmail.com>
2026-03-12 14:46:43 +0800

    refactor: revert typescript files to js

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,15 +1,13 @@
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 
-// Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const envFound = dotenv.config();
 if (envFound.error) {
-  // This throws an error if your .env file is missing in the root directory
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
-export default {
+module.exports = {
   port: parseInt(process.env.PORT || "5000", 10),
   databaseURL: process.env.MONGODB_URI,
   logs: {
--- backend/src/loaders/express.js
+++ backend/src/loaders/express.js
@@ -1,17 +1,12 @@
-import express, { Application } from "express";
-// import { Request, Response, NextFunction } from 'express';
-import cors from "cors";
-import helmet from "helmet";
-import compression from "compression";
-import morgan from "morgan";
-import mongoSanitize from "express-mongo-sanitize";
-import hpp from "hpp";
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
 
-interface HttpError extends Error {
-  status?: number;
-}
-
-export default ({ app }: { app: Application }) => {
+module.exports = ({ app }) => {
   /**
    * Health Check endpoints
    */
@@ -22,12 +17,12 @@ export default ({ app }: { app: Application }) => {
     res.status(200).end();
   });
 
-  // Useful if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+  // Useful if you're behind a reverse proxy (Nginx, AWS, etc)
   app.enable("trust proxy");
 
   // The Magic Middlewares
   app.use(helmet()); // Security headers
-  app.use(cors()); // Enable Cross Origin Resource Sharing
+  app.use(cors()); // Enable CORS
   app.use(compression()); // Compress responses
   app.use(morgan("dev")); // HTTP request logger
 
@@ -45,12 +40,13 @@ export default ({ app }: { app: Application }) => {
   /// catch 404 and forward to error handler
   app.use((req, res, next) => {
     const err = new Error("Not Found");
-    res.status(404);
+    err.status = 404;
     next(err);
   });
 
   /// error handlers
-  app.use((err: HttpError, req: express.Request, res: express.Response) => {
+  // Note: You must keep the 4th 'next' parameter for Express to identify this as an error handler
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({
       errors: {
