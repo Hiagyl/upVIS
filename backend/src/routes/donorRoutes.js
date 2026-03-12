9cee2ba80bf6efc9a9d22c3171e05c29bcc2695b
Hiagyl <joshuaticot@gmail.com>
2026-03-12 20:26:35 +0800

    feat: add donor backend api

---
+++ backend/src/routes/donorRoutes.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const DonorController = require("../controllers/donorController");
+
+const route = Router();
+
+module.exports = (app) => {
+    app.use("/api/donors", route);
+
+    route.get("/", DonorController.getDonors);
+    route.get("/:id", DonorController.getDonor);
+    route.post("/", DonorController.createDonor);
+    route.put("/:id", DonorController.updateDonor);
+    route.delete("/:id", DonorController.deleteDonor);
+};
