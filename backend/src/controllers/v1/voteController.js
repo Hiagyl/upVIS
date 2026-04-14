af1de89308612bd68a87ea4b006893224ee0e23b
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-04-14 21:08:10 +0800

    feat:backend for student voting
    created models, services, controllers, routes... for the feature above

---
+++ backend/src/controllers/v1/voteController.js
@@ -0,0 +1,42 @@
+const VoteService = require("../../services/v1/voteService");
+
+class VoteController {
+    async castVote(req, res, next) {
+        try {
+            // scholarId comes from session
+            const scholarId = req.session.scholarId;
+            if (!scholarId)
+                return res.status(401).json({ success: false, message: "Unauthorized" });
+
+            const { pollId, selectedOption } = req.body;
+            const vote = await VoteService.castVote(pollId, scholarId, selectedOption);
+            res.status(201).json({ success: true, data: vote });
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    async getVotesByPoll(req, res, next) {
+        try {
+            const votes = await VoteService.getVotesByPoll(req.params.pollId);
+            res.status(200).json({ success: true, count: votes.length, data: votes });
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    async getVoteByScholar(req, res, next) {
+        try {
+            const scholarId = req.session.scholarId;
+            if (!scholarId)
+                return res.status(401).json({ success: false, message: "Unauthorized" });
+
+            const vote = await VoteService.getVoteByScholar(req.params.pollId, scholarId);
+            res.status(200).json({ success: true, data: vote });
+        } catch (err) {
+            next(err);
+        }
+    }
+}
+
+module.exports = new VoteController();
